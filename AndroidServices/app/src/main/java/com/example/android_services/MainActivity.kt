cb1e4384d64184e54cacff7f9701f99192dc7b50
Uwimana Jessy Bandya <jessy.bandya5@gmail.com>
2022-02-21 15:08:36 +0300

    Added Services project.

---
+++ AndroidServices/app/src/main/java/com/example/android_services/MainActivity.kt
@@ -0,0 +1,31 @@
+package com.example.android_services
+
+import android.content.Intent
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.util.Log
+import com.example.android_services.databinding.ActivityMainBinding
+
+class MainActivity : AppCompatActivity() {
+    private lateinit var binding: ActivityMainBinding
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+        val serviceIntent = Intent(this, MyBackgroundService::class.java)
+        serviceIntent.putExtra(MyBackgroundService.NAME,"Alex")
+        serviceIntent.putExtra(MyBackgroundService.MARKS,78)
+        binding.btnStart.setOnClickListener {
+            Log.i(MyBackgroundService.TAG,"Starting service")
+            startService(serviceIntent)
+        }
+
+        binding.btnStop.setOnClickListener {
+            Log.i(MyBackgroundService.TAG,"Stopping service")
+            stopService(serviceIntent)
+        }
+    }
+
+    }
+}
