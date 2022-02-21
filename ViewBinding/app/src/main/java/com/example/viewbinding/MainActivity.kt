97c9181e5ca66aadbc721fbfcc95ed09912a970c
Uwimana Jessy Bandya <jessy.bandya5@gmail.com>
2022-02-21 10:06:46 +0300

    Done Exercise 1.

--- ViewBinding/app/src/main/java/com/example/viewbinding/MainActivity.kt
+++ ViewBinding/app/src/main/java/com/example/viewbinding/MainActivity.kt
@@ -2,10 +2,23 @@ package com.example.viewbinding
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.example.viewbinding.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+    private lateinit var binding : ActivityMainBinding
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        val view = binding.root
+        setContentView(view)
+
+        binding.apply {
+            btnSubmit.setOnClickListener {
+                val name = etName.text.toString()
+                tvMessage.text = "Hello! $name"
+                etName.setText("")
+            }
+        }
     }
 }
---
+++ exercice/RoomCRUDCoroutinesWithViewBinding/app/src/main/java/com/example/roomcrudcoroutineswithviewbinding/StudentViewModelFactory.kt
@@ -0,0 +1,18 @@
+package com.example.roomcrudcoroutineswithviewbinding
+
+
+import androidx.lifecycle.ViewModel
+import androidx.lifecycle.ViewModelProvider
+import com.example.roomcrudcoroutineswithviewbinding.db.StudentDao
+import java.lang.IllegalArgumentException
+
+class StudentViewModelFactory(
+    private val dao: StudentDao
+):ViewModelProvider.Factory {
+    override fun <T : ViewModel?> create(modelClass: Class<T>): T {
+        if(modelClass.isAssignableFrom(StudentViewModel::class.java)){
+            return StudentViewModel(dao) as T
+        }
+        throw IllegalArgumentException("Unknown View Model Class")
+    }
+}
