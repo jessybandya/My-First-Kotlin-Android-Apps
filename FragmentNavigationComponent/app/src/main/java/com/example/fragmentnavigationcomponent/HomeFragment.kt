9d14c7c67118b6d5aefc290f89e61004643726df
Uwimana Jessy Bandya <jessy.bandya5@gmail.com>
2022-02-23 07:08:07 +0300

    Added Fragmentcomponent navigation.

---
+++ FragmentNavigationComponent/app/src/main/java/com/example/fragmentnavigationcomponent/HomeFragment.kt
@@ -0,0 +1,31 @@
+package com.example.fragmentnavigationcomponent
+
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import android.widget.Toast
+import androidx.navigation.findNavController
+import com.example.fragmentnavigationcomponent.databinding.FragmentHomeBinding
+
+
+class HomeFragment : Fragment() {
+    private lateinit var binding: FragmentHomeBinding
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        binding = FragmentHomeBinding.inflate(inflater,container,false)
+        binding.btnSignUp.setOnClickListener {
+            it.findNavController().navigate(R.id.action_homeFragment_to_nameFragment)
+        }
+
+        binding.btnTerms.setOnClickListener {
+            it.findNavController().navigate(R.id.action_homeFragment_to_termsFragment)
+
+        }
+        return binding.root
+    }
+
+}
